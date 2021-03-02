e192d3c2ffcc8928dbb80ebfefbab76f554691b9
Kritika <kritikabindal30@gmail>
2021-03-02 23:14:57 +0530

    LL

--- DS&ALGO/LinkedList/src/ll/LinkedList.java
+++ DS&ALGO/LinkedList/src/ll/LinkedList.java
@@ -136,4 +136,85 @@ public Node getNodeAt(int idx) throws Exception
 	}
 	return temp;
 }
+/* Add at a given Position */
+public void addAt(int item ,int idx) throws Exception
+{
+	if(idx<0|| idx > size)
+	{
+		throw new Exception("Invalid Index");
+	}
+	if(idx==0)
+	{
+		addFirst(item);
+	}
+	else if(idx==this.size)
+	{
+		addLast(item);
+	}
+	else
+	{
+		Node nn=new Node();
+		nn.data=item;
+		nn.next=null;
+		
+		//attachment
+		Node nm1=getNodeAt(idx-1);
+		Node np1=nm1.next;
+		
+		nm1.next=nn;
+		nn.next=np1;
+		
+		//summary object
+		this.size++;
+	}
+	
+}
+/* Remove the item from first */
+
+public int removeFirst() throws Exception
+{
+	if(this.size==0)
+	{
+		throw new Exception("LL is empty");
+	}
+	int rv=this.head.data;
+	if(this.size==1)
+	{
+		this.head=null;
+		this.tail=null;
+		this.size=0;
+	}
+	else
+	{
+		this.head=this.head.next;
+		this.size--;
+	}
+	return rv;
+}
+/*Remove the item from the last */
+public int removeLast() throws Exception
+{
+
+	if(this.size==0)
+	{
+		throw new Exception("LL is empty");
+	}
+	int rv=this.tail.data;
+	
+	if(this.size==1)
+	{
+		this.head=null;
+		this.tail=null;
+		this.size=0;
+	}
+	else
+	{
+		Node sizem2= getNodeAt(this.size-2);
+		this.tail=sizem2;
+		this.tail.next=null;
+		this.size--;
+	}
+	return rv;
+}
+
 }
