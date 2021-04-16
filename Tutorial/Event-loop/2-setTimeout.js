55e7f368ff54ee2b3382808dd0380caff75e142d
Kritika <kritikabindal30@gmail.com>
2021-04-16 21:28:32 +0530

    Practice

---
+++ Tutorial/Event-loop/2-setTimeout.js
@@ -0,0 +1,8 @@
+//started operating system process
+console.log('first')
+setTimeout(()=>
+{
+    console.log('second')
+},1000)
+console.log('third')
+//completed and exited operating system Process
