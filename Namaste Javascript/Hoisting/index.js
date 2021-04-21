d68582488c9e13d4b1b2d6d50d34ea7d88e9de38
Kritika <kritikabindal30@gmail.com>
2021-04-21 23:49:16 +0530

    Practice

---
+++ Namaste Javascript/Hoisting/index.js
@@ -0,0 +1,45 @@
+//--------------------------------
+// getName();
+// console.log(x); //undefined
+// console.log(getName);
+
+//normal Function
+// var x=7;
+// function getName()
+// {
+//     console.log("Namaste Javascript")
+// }
+
+
+// getName();
+// console.log(x);
+
+
+
+//----------------------------------------
+//if its arrow function
+
+// getName();
+// console.log(getName);//we get undefined
+// console.log(x); //we get undefined
+
+// var x=7;
+
+// var getName =()=>
+// {
+//     console.log("Namaste Javascript");
+// }
+
+
+
+//-----------------------------------
+
+// getName();
+console.log(getName);//undefined
+console.log(x); //undefined
+var x=5;
+
+var getName= function()
+{
+    console.log("Namaste Javascript")
+}
