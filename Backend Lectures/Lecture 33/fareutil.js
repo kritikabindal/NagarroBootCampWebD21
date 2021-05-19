a59f74b3ed56a12a3d33b5048333e8df29c85770
Kritika <kritikabindal30@gmail.com>
2021-05-19 22:43:15 +0530

    lecture

---
+++ Backend Lectures/Lecture 33/fareutil.js
@@ -0,0 +1,7 @@
+
+
+const calculateFare=(km,min)=>
+{
+    let fare=50;
+    fare+= km
+}
---
+++ Backend Lectures/Lecture 33/server.js
@@ -0,0 +1,13 @@
+const express =require('express');
+const app=express();
+const path=require('path');
+const calculateFare=require
+
+
+
+
+
+app.post('/calfare',(req,res)=>
+{
+const km=req.body.
+})
